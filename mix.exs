53e9daf5986e33e23bde6b709679d5343c6344b8
Mike Zazaian <mike@functionhaus.com>
2019-09-10 14:51:20 -0400

    add initial mixfile

--- mix.exs
+++ mix.exs
@@ -1,28 +1,58 @@
 defmodule Archivist.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "Plain-text, version-controlled blogging in Arcdown and Markdown."
+
   def project do
     [
       app: :archivist,
-      version: "0.1.0",
+      name: "Archivist",
+      version: @version,
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      build_embedded: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env),
+      deps: deps(),
+      description: @description,
+      package: package(),
+      source_url: "https://github.com/functionhaus/archivist",
+      homepage_url: "https://functionhaus.com",
+      docs: [
+        logo: "assets/functionhaus_logo.png",
+        extras: ["README.md"],
+        main: "readme",
+        source_ref: "v#{@version}",
+        source_url: "https://github.com/functionhaus/archivist"
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
+
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:arcdown, "~> 0.1"},
+      {:ex_doc, "~> 0.19", only: :docs}
     ]
+  end
+
+  defp package do
+    [
+     files: ["lib", "mix.exs", "README.md"],
+     maintainers: ["FunctionHaus LLC, Mike Zazaian"],
+     licenses: ["Apache 2"],
+
+     links: %{"GitHub" => "https://github.com/functionhaus/archivist",
+              "Docs" => "https://hexdocs.pm/archivist/"}
+     ]
   end
 end
