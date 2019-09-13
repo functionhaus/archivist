95e7c303bbe89c0da596bbea5aca9f478afe73ca
Mike Zazaian <mike@functionhaus.com>
2019-09-12 20:56:25 -0400

    add initial archivist public interface

--- lib/archivist.ex
+++ lib/archivist.ex
@@ -1,27 +1,105 @@
 defmodule Archivist do
   @moduledoc """
-  Documentation for Archivist.
+  Module that coordinates the structured parsing and return of assets in the
+  articles root directory.
+
+  Precompiles and provides interface to interact with your articles.
+
+  defmodule MyApp.Archive do
+    use Archivist.Archive,
+      root: "some/other/dir",
+      body_parser: Earmark
+
+  end
+
+  alias MyApp.Archive
+
+  {:ok, articles} = Archive.all
+  {:ok, authors} = Archive.authors
+  {:ok, created_asc} = Archive.sort_by :created_at, :asc
+  {:ok, published_desc} = Archive.sort_by :published_at, :desc
+  {:ok, tags} = Archive.tags
+  {:ok, topics} = Archive.topics
+  {:ok, titles} = Archive.titles
+  {:ok, slugs} = Archive.slugs
+
+  {:ok, article} = Archive.fetch_by :slug, "some-article"
   """
 
-  @content_dir "priv/articles"
-  @match_pattern "**/*.ad"
+  require Logger
 
-  # @articles
+  @doc false
+  defmacro __using__(options) do
+    quote bind_quoted: [options: options], unquote: true do
+      @defaults %{
+        content_dir: "priv/articles",
+        match_pattern: "**/*.ad",
+        content_parser: Earmark,
+        article_parser: Arcdown
+      }
 
-  # @topics
+      @settings Map.merge(@defaults, options)
 
-  # @tags
+      @before_compile unquote(__MODULE__)
+    end
+  end
 
-  # @authors
+  @doc false
+  defmacro __before_compile__(env) do
+    settings = Module.get_attribute(env.module, :settings)
 
-  def article_paths do
-    article_glob()
+    # quote individual article paths as external resources so that they can be
+    # tracked later for autoloading, etc.
+    #
+    content_dir = settings[:content_dir]
+    match_pattern = settings[:match_pattern]
+
+    article_paths = article_paths(content_dir, match_pattern)
+    articles_stream = articles_stream(article_paths)
+
+    external_resources = article_paths
+      |> Enum.map(&quote(do: @external_resource unquote(&1)))
+
+    quote do
+      unquote(external_resources)
+
+      def articles do
+        unquote Enum.to_list(articles_stream)
+      end
+
+      def topics do
+        unquote parse_attrs(:topics, articles_stream)
+      end
+
+      def tags do
+        unquote parse_attrs(:tags, articles_stream)
+      end
+
+      def authors do
+        unquote parse_attrs(:author, articles_stream)
+      end
+    end
+  end
+
+  defp article_paths(content_dir, pattern) do
+    content_dir
+    |> Path.relative_to_cwd
+    |> Path.join([pattern])
     |> Path.wildcard
   end
 
-  def article_glob do
-    @content_dir
-    |> Path.relative_to_cwd
-    |> Path.join([@match_pattern])
+  defp articles_stream(article_paths) do
+    Stream.map(article_paths, fn path ->
+      {:ok, parsed} = Arcdown.parse_file(path)
+      parsed
+    end)
+  end
+
+  defp parse_attrs(attr, articles_stream) do
+    articles_stream
+    |> Stream.flat_map(fn article -> article[attr] end)
+    |> Stream.reject(&is_nil/1)
+    |> Stream.uniq
+    |> Enum.sort
   end
 end
--- test/archivist_test.exs
+++ test/archivist_test.exs
@@ -1,11 +1,7 @@
 defmodule ArchivistTest do
   use ExUnit.Case, async: false
 
-  test "constructs the correct matcher glob" do
-    assert Archivist.article_glob() =~ "priv/articles/**/*.ad"
-  end
-
-  test "constructs a list of article paths" do
-    assert Archivist.article_paths() == ["priv/articles/the_day_the_earth_stood_still.ad"]
+  test "parses a list of articles" do
+    assert Archivist.articles == "some articles"
   end
 end
