ee9f2ca3ab4899dcf23f578e6706a1e45509bad5
Mike Zazaian <mike@functionhaus.com>
2019-09-19 14:30:14 -0400

    bump minor version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Archivist.MixProject do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.2.1"
   @description "Plain-text, version-controlled blogging in Arcdown and Markdown."
 
   def project do
